d85d9251c76553dc075580f6773c12499d24c3d9
offthecliff46git <offthecliff46@gmail.com>
2019-05-01 12:35:44 -0400

    seed file

---
+++ server/db/models/ticket.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Ticket = db.define('ticket', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  points: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 1,
+    },
+  },
+  status: {
+    type: Sequelize.ENUM('to do', 'in progress', 'in review', 'done'),
+    allowNull: false,
+  },
+});
+
+module.exports = Ticket;

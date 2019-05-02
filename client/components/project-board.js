0a0f6735a844ab564f91cfa6cb653c9eac54c6a1
Christina Armstrong <armstrongch6@gmail.com>
2019-05-02 17:01:38 -0400

    continued work on drag and drop functionality with dummy data

--- client/components/project-board.js
+++ client/components/project-board.js
@@ -3,70 +3,64 @@ import { Container, Row, Col } from 'reactstrap';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Ticket from './ticket';
 import StatusColumn from './status-column';
-const toDoTickets = [
-  {
+const tickets = {
+  '1': {
     id: 1,
     title: 'create User',
     desc: 'idk',
     points: 1,
     status: 'to do'
   },
-  {
+  '2': {
     id: 2,
     title: 'create footer',
     desc: 'idk2',
     points: 4,
     status: 'to do'
-  }
-];
-const inProgressTickets = [
-  {
+  },
+  '3': {
     id: 3,
     title: 'create nav',
     desc: 'idk',
     points: 1,
     status: 'in progress'
   },
-  {
+  '4': {
     id: 4,
     title: 'create footer',
     desc: 'idk2',
     points: 3,
     status: 'in progress'
-  }
-];
-const inReview = [
-  {
+  },
+  '5': {
     id: 5,
     title: 'have fun',
     desc: 'idk',
     points: 1,
     status: 'in review'
   },
-  {
+  '6': {
     id: 6,
     title: 'create footer',
     desc: 'idk2',
     points: 3,
     status: 'in review'
-  }
-];
-const done = [
-  {
+  },
+  '7': {
     id: 7,
     title: 'have fun',
     desc: 'idk',
     points: 1,
     status: 'done'
   },
-  {
+  '8': {
     id: 8,
     title: 'create footer',
     desc: 'idk2',
     points: 3,
     status: 'done'
   }
-];
+};
 
 export default class ProjectBoard extends React.Component {
   constructor() {
@@ -75,25 +69,24 @@ export default class ProjectBoard extends React.Component {
       columns: {
         '1': {
           id: 1,
-          taskIds: []
+          taskIds: [1, 2]
         },
         '2': {
           id: 2,
-          taskIds: [0, 1]
+          taskIds: [3, 4]
         },
         '3': {
           id: 3,
-          taskIds: []
+          taskIds: [5, 6]
         },
         '4': {
           id: 4,
-          taskIds: []
+          taskIds: [7, 8]
         }
       }
     };
   }
   onDragEnd = result => {
-    console.log('RESULT --> ', result);
     const { destination, source, draggableId } = result;
 
     if (!destination) {
@@ -107,21 +100,50 @@ export default class ProjectBoard extends React.Component {
       return;
     }
 
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds
+      };
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn
+        }
+      };
+      this.setState(newState);
+      return;
+    }
+
+    const startTaskIds = Array.from(start.taskIds);
+
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds
     };
-    //retrieve the column
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds
+    };
+
     const newState = {
       ...this.state,
       columns: {
         ...this.state.columns,
-        [newColumn.id]: newColumn
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish
       }
     };
     this.setState(newState);
@@ -140,45 +162,16 @@ export default class ProjectBoard extends React.Component {
             <Col>Done</Col>
           </Row>
           <Row className="board-container">
-            <Droppable droppableId="1">
+            <Droppable droppableId="1" style={{ height: '100% !important' }}>
               {provided => (
                 <Col>
                   <StatusColumn
                     provided={provided}
                     innerRef={provided.innerRef}
                   >
-                    {toDoTickets.map(ticket => {
+                    {this.state.columns['1'].taskIds.map((ticketId, index) => {
+                      const ticket = tickets[ticketId];
                       return (
-                        <Draggable draggableId={ticket.id} index={ticket.id}>
-                          {provided => (
-                            <Ticket
-                              provided={provided}
-                              innerRef={provided.innerRef}
-                              ticket={ticket}
-                            />
-                          )}
-                        </Draggable>
-                      );
-                    })}
-                    {provided.placeholder}
-                  </StatusColumn>
-                </Col>
-              )}
-            </Droppable>
-            <Droppable droppableId="2">
-              {provided => (
-                <Col>
-                  <StatusColumn
-                    provided={provided}
-                    innerRef={provided.innerRef}
-                  >
-                    {this.state.columns['2'].taskIds.map((ticketId, index) => {
-                      console.log('taskIds', this.state.columns['2'].taskIds);
-                      console.log('ticketId', ticketId);
-                      const ticket = inProgressTickets[ticketId];
-                      console.log('TICKET --> ', ticket);
-                      return (
-                        // this index needs to be ordered
                         <Draggable draggableId={ticket.id} index={index}>
                           {provided => (
                             <Ticket
@@ -195,16 +188,17 @@ export default class ProjectBoard extends React.Component {
                 </Col>
               )}
             </Droppable>
-            <Droppable droppableId="3">
+            <Droppable droppableId="2" style={{ height: '100%' }}>
               {provided => (
                 <Col>
                   <StatusColumn
                     provided={provided}
                     innerRef={provided.innerRef}
                   >
-                    {inReview.map(ticket => {
+                    {this.state.columns['2'].taskIds.map((ticketId, index) => {
+                      const ticket = tickets[ticketId];
                       return (
-                        <Draggable draggableId={ticket.id} index={ticket.id}>
+                        <Draggable draggableId={ticket.id} index={index}>
                           {provided => (
                             <Ticket
                               provided={provided}
@@ -220,16 +214,43 @@ export default class ProjectBoard extends React.Component {
                 </Col>
               )}
             </Droppable>
-            <Droppable droppableId="4">
+            <Droppable droppableId="3" style={{ height: '100%' }}>
               {provided => (
                 <Col>
                   <StatusColumn
                     provided={provided}
                     innerRef={provided.innerRef}
                   >
-                    {done.map(ticket => {
+                    {this.state.columns['3'].taskIds.map((ticketId, index) => {
+                      const ticket = tickets[ticketId];
                       return (
-                        <Draggable draggableId={ticket.id} index={ticket.id}>
+                        <Draggable draggableId={ticket.id} index={index}>
+                          {provided => (
+                            <Ticket
+                              provided={provided}
+                              innerRef={provided.innerRef}
+                              ticket={ticket}
+                            />
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </StatusColumn>
+                </Col>
+              )}
+            </Droppable>
+            <Droppable droppableId="4" style={{ height: '100%' }}>
+              {provided => (
+                <Col>
+                  <StatusColumn
+                    provided={provided}
+                    innerRef={provided.innerRef}
+                  >
+                    {this.state.columns['4'].taskIds.map((ticketId, index) => {
+                      const ticket = tickets[ticketId];
+                      return (
+                        <Draggable draggableId={ticket.id} index={index}>
                           {provided => (
                             <Ticket
                               provided={provided}

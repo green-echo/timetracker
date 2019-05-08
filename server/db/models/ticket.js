4c894f6d5fc77298bb33fa1407b18d350801fa8e
Ariel Ahdoot <arieli612@aol.com>
2019-05-08 15:50:23 -0400

    changed ticket model to add order

--- server/db/models/ticket.js
+++ server/db/models/ticket.js
@@ -1,34 +1,40 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Ticket = db.define('ticket', { 
+const Ticket = db.define('ticket', {
   title: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true, 
-    },
+      notEmpty: true
+    }
   },
   description: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true,
-    },
+      notEmpty: true
+    }
   },
   points: {
     type: Sequelize.INTEGER,
     allowNull: false,
     validate: {
       notEmpty: true,
-      min: 1,
-    },
+      min: 1
+    }
   },
   status: {
     type: Sequelize.ENUM('to_do', 'in_progress', 'in_review', 'done'),
     allowNull: false,
-    defaultValue: 'to_do' 
+    defaultValue: 'to_do'
   },
+  order: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+    min: 0
+  }
 });
 
 module.exports = Ticket;

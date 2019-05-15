4fb51d55f02695a96f4bebc03a8f417679e0bf95
Christina Armstrong <armstrongch6@gmail.com>
2019-05-15 12:37:39 -0400

    pie chart labels added to pie chart

--- client/components/PieChart.js
+++ client/components/PieChart.js
@@ -7,24 +7,62 @@ import { getProjectThunk } from '../actions/project';
 import { arc as d3Arc, pie as d3Pie } from 'd3-shape';
 import Slice from './Slice';
 
+let width;
+let height;
+let minViewportSize;
+let radius;
+let x;
+let y;
 class PieChart extends Component {
   componentDidMount() {
+    this.calculatePosition();
     this.props.getUsersOnProj();
     this.props.getProject();
+    window.addEventListener('resize', this.calculatePosition);
   }
+  componentDidUpdate(prevProps) {
+    console.log('PREVPROPS INSIDE PIECHART', prevProps);
+    this.calculatePosition();
+  }
+  calculatePosition() {
+    console.log('THIS IS GETTING CALLED');
+    width = window.innerWidth;
+    height = window.innerHeight;
+    minViewportSize = Math.min(width, height);
+
+    radius = minViewportSize * 0.9 / 2;
+
+    x = width / 2;
+    y = height / 2;
+    console.log('x', x);
+    console.log('y', y);
+  }
+
   render() {
-    console.log('DATA', this.props.userdata);
-    const height = 500;
-    const width = 500;
+    // let width = window.innerWidth;
+    // let height = window.innerHeight;
+    // let minViewportSize = Math.min(width, height);
+
+    // let radius = minViewportSize * 0.9 / 2;
+
+    // let x = width / 2;
+    // let y = height / 2;
 
     if (this.props.userdata.length) {
       let data = this.props.userdata.map(object => object.points);
       console.log('DATA!!!', data);
       let pie = d3.pie()(data);
       return (
-        <svg height={height} width={width}>
-          <g transform={`translate(${width / 2}, ${height / 2})`}>
-            <Slice pie={pie} alldata={this.props.userdata} />
+        <svg height="100vh" width="100%">
+          <g transform={`translate(${x}, ${y})`}>
+            <Slice
+              pie={pie}
+              x={x}
+              y={y}
+              radius={radius}
+              cornerRadius={7}
+              alldata={this.props.userdata}
+            />
           </g>
         </svg>
       );
--- client/components/Slice.js
+++ client/components/Slice.js
@@ -3,49 +3,41 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 
 class Slice extends Component {
+  componentDidUpdate(prevProps) {
+    console.log('component was updated');
+    console.log('PREVPROPS', prevProps);
+  }
   render() {
-    let { pie } = this.props;
-    console.log('PIES', pie);
+    let { pie, x, y } = this.props;
+    console.log('x insice slice', x);
+    console.log('y inside of slice', y);
     let arc = d3
       .arc()
-      .innerRadius(0)
-      .outerRadius(100);
+      .innerRadius(50)
+      .outerRadius(this.props.radius);
     let interpolate = d3.interpolateRgb('#eaaf79', '#bc3358');
 
     return pie.map((slice, index) => {
-      console.log('SLICE', slice);
-      console.log('data', this.props.alldata);
       let sliceColor = interpolate(index / (pie.length - 1));
       const user = this.props.alldata.find(
         object => object.points === slice.data
       );
-      console.log('USER!!', user);
+
+      console.log('slice!!', slice);
+      if (user.user === null) {
+        user.user = 'unassigned';
+      }
       return (
         <React.Fragment>
           <path d={arc(slice)} fill={sliceColor} />
-          {/* <text
-            transform={`translate(${arc.centroid(slice)})`}
-            fill="white"
-            dy=".35em"
-          >
-            {slice.value}
-          </text>> */}
-          {/* {this.props.alldata.map(object => {
-            return ( */}
+
           <text
             transform={`translate(${arc.centroid(slice)})`}
-            fill="white"
-            dy=".25em"
+            fill="black"
+            dy=".35em"
           >
             {user.user}
           </text>
-          {/* );
-          })} */}
-          {/* {Object.keys(this.props.alldata)
-            .filter(key => this.props.alldata[key] === slice.data)
-            .map((key, index) => {
-              return <text key={key}> {this.props.alldata[key].user}</text>;
-            })} */}
         </React.Fragment>
       );
     });

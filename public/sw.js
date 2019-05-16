f37afd4266739a2539bee371f5ced2ab440306f0
Ariel Ahdoot <arieli612@aol.com>
2019-05-15 23:01:01 -0400

    readme

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,5 @@
 var cacheName = 'cache';
 var filesToCache = [
-  
   './404-error-page-examples.jpeg',
   './bundle.js',
   './manifest.json',
@@ -11,21 +10,17 @@ var filesToCache = [
 
   './timey-icon.png',
   './index.html',
-  './timeylogo.jpeg',
-
-
+  './timeylogo.jpeg'
 ];
 
-
-
-self.addEventListener("install", function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
 });
 
-var preLoad = function(){
-  console.log("Installing web app");
+var preLoad = function() {
+  console.log('Installing web app');
   return caches.open(cacheName).then(function(cache) {
-    console.log("caching index and important routes");
+    console.log('caching index and important routes');
     return cache.addAll(filesToCache);
   });
 };
@@ -39,7 +34,7 @@ self.addEventListener('activate', function(event) {
             return caches.delete(cName);
           }
         })
-      );  
+      );
     })
   );
 });
@@ -47,18 +42,19 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
+      console.log('[Service Worker] Fetching resource: ' + e.request.url);
+      return (
+        r ||
+        fetch(e.request).then(function(response) {
+          return caches.open(cacheName).then(function(cache) {
+            console.log(
+              '[Service Worker] Caching new resource: ' + e.request.url
+            );
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
     })
   );
 });
-
-
-
-
